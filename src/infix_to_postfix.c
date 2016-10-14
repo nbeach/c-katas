b2eae62ec74aeeeb902d0c3a36c17977c14a1120
Nicholas Beach <nicholascbeach@gmail.com>
2016-10-14 18:35:43 -0400

    Added infix_to_postfix converts expressions with only subtraction test

--- src/infix_to_postfix.c
+++ src/infix_to_postfix.c
@@ -2,6 +2,36 @@
 #include <string.h>
 #include "infix_to_postfix.h"
 
-void infix_to_postfix(char *infix, char* postfix) {
-  strcpy(postfix, "abc++");
+bool is_variable(char value);
+void append_char(char *dest, char value);
+
+void infix_to_postfix(char *infix, char *postfix) {
+  postfix[0] = '\0';
+
+  int length = strlen(infix);
+  char operators[length / 2];
+  operators[0] = '\0';
+
+  for(int i = 0; i < length; i++) {
+    char value = infix[i];
+
+    if(is_variable(value)) {
+      append_char(postfix, value);
+    } else {
+      append_char(operators, value);
+    }
+  }
+
+  strcat(postfix, operators);
+}
+
+bool is_variable(char value) {
+  return value >= 'a' && value <= 'z';
+}
+
+void append_char(char *dest, char value) {
+  char append[2];
+  append[0] = value;
+  append[1] = '\0';
+  strcat(dest, append);
 }
--- test/infix_to_postfix_test.c
+++ test/infix_to_postfix_test.c
@@ -4,7 +4,7 @@
 
 #include "../src/infix_to_postfix.h"
 
-START_TEST (infix_to_postfix_converts_expressions_with_only_addition_of_variables)
+START_TEST (infix_to_postfix_converts_expressions_with_only_addition)
 {
   char actual[10];
   infix_to_postfix("a+b+c", actual);
@@ -12,11 +12,20 @@ START_TEST (infix_to_postfix_converts_expressions_with_only_addition_of_variable
 }
 END_TEST
 
+START_TEST (infix_to_postfix_converts_expressions_with_only_subtraction)
+{
+  char actual[10];
+  infix_to_postfix("a-b-c", actual);
+  ck_assert_str_eq("abc--", actual);
+}
+END_TEST
+
 Suite * infix_postfix_suite() {
   Suite *s = suite_create("infix_to_postfix_test");
   TCase *tc_core = tcase_create("Core");
 
-  tcase_add_test(tc_core, infix_to_postfix_converts_expressions_with_only_addition_of_variables);
+  tcase_add_test(tc_core, infix_to_postfix_converts_expressions_with_only_addition);
+  tcase_add_test(tc_core, infix_to_postfix_converts_expressions_with_only_subtraction);
 
   suite_add_tcase(s, tc_core);
   return s;
