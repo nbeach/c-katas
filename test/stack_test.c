21c091e712f8e8e007736efb14b62fdbabd9c259
Nicholas Beach <nicholascbeach@gmail.com>
2016-10-14 20:35:00 -0400

    Simplified stack tests setup

--- test/stack_test.c
+++ test/stack_test.c
@@ -6,10 +6,7 @@
 
 START_TEST (char_stack_push_adds_characters_to_top_of_stack)
 {
-  char stack[5];
-  stack[0] = 'a';
-  stack[1] = 'b';
-  stack[2] = '\0';
+  char stack[5] = "ab";
   char_stack_push(stack, 'c');
   ck_assert_str_eq("abc", stack);
 }
@@ -17,10 +14,7 @@ END_TEST
 
 START_TEST (char_stack_push_adds_terminator_past_end_of_stack)
 {
-  char stack[5];
-  stack[0] = 'a';
-  stack[1] = 'b';
-  stack[2] = '\0';
+  char stack[5] = "ab";
   char_stack_push(stack, 'c');
   ck_assert_int_eq('\0', stack[3]);
 }
@@ -28,43 +22,29 @@ END_TEST
 
 START_TEST (char_stack_peek_returns_the_value_on_top_of_the_stack)
 {
-  char stack[5];
-  stack[0] = 'a';
-  stack[1] = 'b';
-  stack[2] = '\0';
+  char stack[5] = "ab";
   ck_assert_int_eq('b', char_stack_peek(stack));
 }
 END_TEST
 
 START_TEST (char_stack_peek_does_not_modify_the_stack)
 {
-  char stack[5];
-  stack[0] = 'a';
-  stack[1] = 'b';
-  stack[2] = '\0';
+  char stack[5] = "ab";
   char_stack_peek(stack);
-  ck_assert_int_eq('a', stack[0]);
-  ck_assert_int_eq('b', stack[1]);
-  ck_assert_int_eq('\0', stack[2]);
+  ck_assert_str_eq("ab", stack);
 }
 END_TEST
 
 START_TEST (char_stack_pop_returns_the_value_on_top_of_the_stack)
 {
-  char stack[5];
-  stack[0] = 'a';
-  stack[1] = 'b';
-  stack[2] = '\0';
+  char stack[5] = "ab";
   ck_assert_int_eq('b', char_stack_pop(stack));
 }
 END_TEST
 
 START_TEST (char_stack_pop_removes_the_value_on_top_of_the_stack)
 {
-  char stack[5];
-  stack[0] = 'a';
-  stack[1] = 'b';
-  stack[2] = '\0';
+  char stack[5] = "ab";
   char_stack_pop(stack);
   ck_assert_int_eq('\0', stack[1]);
 }
